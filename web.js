dd597f9ac0244b727b1ac4f61a2656641f0ce979
Amol Gaikwad <gaikwad.amol04@gmail.com>
2013-07-09 19:53:05 +0000

    Hello

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buffer = new Buffer(350);
+var buffer = new Buffer(3500);
 
 buffer=fs.readFileSync('/home/ubuntu/bitstarter/index.html');
-var out=buffer.toString('utf-8');
-
+//var out=buffer.toString('utf-8');
+var out="Hello\n";
 
 
 app.get('/', function(request, response) {
