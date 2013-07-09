7c848b6af1494af453ba495f5c252804c64dd8a3
Amol Gaikwad <gaikwad.amol04@gmail.com>
2013-07-09 20:21:40 +0000

    Revert

--- web.js
+++ web.js
@@ -1,18 +1,9 @@
-
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
-var buffer = new Buffer(3500);
-
-buffer=fs.readFileSync('/home/ubuntu/bitstarter/index.html');
-//var out=buffer.toString('utf-8');
-var out="Hello\n";
-
-
 app.get('/', function(request, response) {
-  response.send('The response is ');
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
