a38adec4c54ca429e4ba098b89350638bb78ca5e
Amol Gaikwad <gaikwad.amol04@gmail.com>
2013-07-10 15:18:51 +0000

    New start

--- web.js
+++ web.js
@@ -2,11 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+var buffer = new Buffer(350);
+
+buffer=fs.readFileSync('/home/ubuntu/bitstarter/index.html');
+var out=buffer.toString('utf-8');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
